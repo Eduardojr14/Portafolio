b18bca78eb01730a8114a08b688e43537d139406
Eduardojr14 <eduardojr1402@gmail.com>
2022-09-12 15:22:54 -0300

    version3.0

---
+++ JS/formulario.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let formulario = document.getElementById('formul');
+    formulario.addEventListener('submit', function() {
+        formulario.reset();
+    });
+});
